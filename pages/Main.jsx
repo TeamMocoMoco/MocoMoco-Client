768aec5b76040c2832930e4fddd651bbf11f865b
Delilah1004 <pskdy8414@naver.com>
2021-04-26 14:30:21 +0900

    add: Tab Screens

---
+++ pages/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { getStatusBarHeight } from "react-native-status-bar-height"
+import { StyleSheet, Text, View } from "react-native"
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text>Main</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: getStatusBarHeight(),
+    justifyContent: "center",
+  },
+  content: {
+    alignItems: "center",
+  },
+})
