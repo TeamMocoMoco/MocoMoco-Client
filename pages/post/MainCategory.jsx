84aa18eb418607eb6fd6da733f4abc772904d646
Delilah1004 <pskdy8414@naver.com>
2021-04-27 15:11:44 +0900

    refactor: 안쓰는 import 제거

--- pages/post/MainCategory.jsx
+++ pages/post/MainCategory.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function MainCategory() {
