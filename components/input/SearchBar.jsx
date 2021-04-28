1257caa4abe89eb3f04e367c498cd73129b001a3
Delilah1004 <pskdy8414@naver.com>
2021-04-28 15:53:02 +0900

    style: SearchBar

--- components/input/SearchBar.jsx
+++ components/input/SearchBar.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
 export default function SearchBar() {
   const [value, setValue] = useState('알고리즘');
@@ -8,16 +14,16 @@ export default function SearchBar() {
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-          style={{ color: 'white' }}
+          style={styles.input}
           value={value}
           onChangeText={(text) => {
             setValue(text);
           }}
         />
-        <Button>
-          <Text>검색</Text>
-        </Button>
       </View>
+      <TouchableHighlight style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>검색</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -25,12 +31,32 @@ export default function SearchBar() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#2293F4',
-    padding: 10,
+    flexDirection: 'row',
     width: '100%',
+    padding: 10,
   },
   inputContainer: {
     backgroundColor: '#339DFF',
-    padding: 10,
+    flex: 1,
+    height: 40,
+    marginEnd: 10,
     borderRadius: 5,
+  },
+  input: {
+    width: '85%',
+    color: 'white',
+    marginStart: 15,
+    fontSize: 16,
+    flex: 1,
+  },
+  buttonContainer: {
+    backgroundColor: '#1EA7F8',
+    width: '15%',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
   },
 });
