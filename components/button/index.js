d6984ece9c4848f1cd735e18f77eb19f7d2a7a0d
Delilah1004 <pskdy8414@naver.com>
2021-04-28 23:53:02 +0900

    feat: add index FullButton

--- components/button/index.js
+++ components/button/index.js
@@ -1,8 +1,10 @@
+import FullButton from './FullButton';
 import BigCategoryButton from './BigCategoryButton';
 import SmallCategoryButton from './SmallCategoryButton';
 import HashtagButton from './HashtagButton';
 
 module.exports = {
+  FullButton,
   BigCategoryButton,
   SmallCategoryButton,
   HashtagButton,
