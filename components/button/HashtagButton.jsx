9c9a4031e30e13bd6ff5805589ee0533bd36a3ce
Delilah1004 <pskdy8414@naver.com>
2021-04-28 15:51:22 +0900

    add: HashtagButton

---
+++ components/button/HashtagButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default function HashtagButton({ title }) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.title}># {title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#777',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginEnd: 10,
+    borderRadius: 5,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 10,
+  },
+});
