c18e198f21deea8335defb676c076509da352d9e
Delilah1004 <pskdy8414@naver.com>
2021-04-28 15:51:49 +0900

    add: components/button index

---
+++ components/button/index.js
@@ -0,0 +1,9 @@
+import BigCategoryButton from './BigCategoryButton';
+import SmallCategoryButton from './SmallCategoryButton';
+import HashtagButton from './HashtagButton';
+
+module.exports = {
+  BigCategoryButton,
+  SmallCategoryButton,
+  HashtagButton,
+};
