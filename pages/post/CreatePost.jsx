f0ed7f39a91ae8798644674554a7ab5c8b2fab33
Delilah1004 <pskdy8414@naver.com>
2021-05-04 01:46:32 +0900

    style: CreatePost

--- pages/post/CreatePost.jsx
+++ pages/post/CreatePost.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import {
+  Button,
   ScrollView,
   StyleSheet,
   Text,
@@ -9,23 +10,29 @@ import {
   View,
 } from 'react-native';
 
+import { Entypo } from '@expo/vector-icons';
+
 import { Picker } from '@react-native-picker/picker';
 
 import { HeaderBack } from '../../components/header';
 import { SignInput } from '../../components/input';
-import { HashtagButton, FullButton } from '../../components/button';
-
-import { postPosts } from '../../config/PostAPI';
-import kind from '../../config/mock/category.json';
+import {
+  HashtagButton,
+  FullButton,
+  OnAndOffButton,
+} from '../../components/button';
 import DatePickModal from '../../components/modal/DatePickModal';
+
+import kind from '../../config/mock/category.json';
+import { postPosts } from '../../config/PostAPI';
 
 export default function CreatePost({ navigation }) {
   const categoryList = kind.category;
 
-  const [onAndOff, setOnAndOff] = useState('온라인');
+  const [onAndOff, setOnAndOff] = useState('');
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('선택');
-  const [personnel, setPersonnel] = useState('');
+  const [personnel, setPersonnel] = useState(0);
   const [location, setLocation] = useState([36.7, 37.7]);
   const [startDate, setStartDate] = useState('');
   const [dueDate, setDueDate] = useState('');
@@ -40,6 +47,7 @@ export default function CreatePost({ navigation }) {
 
   const upload = async () => {
     await postPosts(
+      navigation,
       onAndOff,
       title,
       category,
@@ -55,7 +63,7 @@ export default function CreatePost({ navigation }) {
   };
 
   const showAddButton = () => {
-    if (hashtagList.length == 3) {
+    if (hashtagList.length == 5) {
       return (
         <TouchableOpacity
           disabled
@@ -109,7 +117,7 @@ export default function CreatePost({ navigation }) {
         <DatePickModal
           modalOpen={modalOpen}
           setModalOpen={setModalOpen}
-          setDate={setStartDate}
+          setDateTime={setStartDate}
         />
       );
     } else {
@@ -117,8 +125,48 @@ export default function CreatePost({ navigation }) {
         <DatePickModal
           modalOpen={modalOpen}
           setModalOpen={setModalOpen}
-          setDate={setDueDate}
+          setDateTime={setDueDate}
         />
+      );
+    }
+  };
+
+  const deleteHashtag = useCallback(async (index) => {
+    let list = hashtagList;
+    list.slice(index, 1);
+    setHashtagList(hashtagList);
+  });
+
+  const onRemove = (idex) => (e) => {
+    setHashtagList(hashtagList.filter((hashtag) => hashtag.idex !== idex));
+  };
+
+  const showSelectedHashtag = () => {
+    if (hashtagList.length > 0) {
+      return (
+        <View style={{ marginBottom: 10 }}>
+          <Text
+            style={{
+              color: '#263238',
+              fontWeight: 'bold',
+              marginVertical: 10,
+            }}
+          >
+            해시태그
+          </Text>
+          <View style={{ flexDirection: 'row' }}>
+            {hashtagList.map((tag, i) => {
+              return (
+                <HashtagButton
+                  title={tag}
+                  index={i}
+                  onRemove={onRemove}
+                  key={i}
+                />
+              );
+            })}
+          </View>
+        </View>
       );
     }
   };
@@ -129,98 +177,203 @@ export default function CreatePost({ navigation }) {
       <ScrollView>
         <View style={styles.content}>
           {/* 온/오프라인 */}
-
-          {/* 제목 */}
-          <SignInput
-            label={'제목'}
-            value={title}
-            type={'title'}
-            hint={'제목을 입력하세요.'}
-            setValue={setTitle}
-          />
-
-          <View style={styles.row}>
-            {/* 모집분류 */}
-            <View style={{ width: '60%' }}>
-              <View style={{ width: '100%', marginBottom: 20 }}>
-                <Text style={styles.label}>모집 분류</Text>
-                <View style={styles.inputBox}>
-                  <Picker
-                    mode="dropdown"
-                    style={styles.picker}
-                    itemStyle={styles.pickerItem}
-                    selectedValue={category}
-                    onValueChange={(value) => setCategory(value)}
-                  >
-                    {categoryList.map((item, i) => {
-                      return <Picker.Item label={item} value={item} key={i} />;
-                    })}
-                  </Picker>
-                </View>
-              </View>
+          <View style={{ width: '100%', marginBottom: 20 }}>
+            <Text style={styles.label}>진행 방식</Text>
+            <View style={{ flexDirection: 'row' }}>
+              {['오프라인', '온라인'].map((title, i) => {
+                return (
+                  <OnAndOffButton
+                    title={title}
+                    onAndOff={onAndOff}
+                    setOnAndOff={setOnAndOff}
+                    key={i}
+                  />
+                );
+              })}
             </View>
+            {/* {message()} */}
+          </View>
 
-            {/* 모집인원 */}
-            <View style={{ width: '35%' }}>
-              <SignInput
-                label={'모집 인원'}
-                value={personnel}
-                type={'personnel'}
-                hint={'모집 인원을 입력하세요.'}
-                setValue={setPersonnel}
+          {/* 모집분류 */}
+          <View
+            style={{
+              flexDirection: 'row',
+              marginBottom: 20,
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 2, marginEnd: 20 }}>
+              <Text style={styles.label}>모집 분류</Text>
+            </View>
+            <View
+              style={{
+                flex: 5,
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                borderRadius: 30,
+                borderWidth: 2,
+                borderColor: '#CBCBCB',
+              }}
+            >
+              <Picker
+                mode="dropdown"
+                style={styles.picker}
+                itemStyle={styles.pickerItem}
+                selectedValue={category}
+                onValueChange={(value) => setCategory(value)}
+              >
+                {categoryList.map((item, i) => {
+                  return <Picker.Item label={item} value={item} key={i} />;
+                })}
+              </Picker>
+            </View>
+          </View>
+
+          {/* 모집인원 */}
+          <View
+            style={{
+              flexDirection: 'row',
+              marginBottom: 20,
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 2, marginEnd: 20 }}>
+              <Text style={styles.label}>모집 인원</Text>
+            </View>
+            <View
+              style={{
+                flex: 5,
+                flexDirection: 'row',
+                marginEnd: '20%',
+                alignItems: 'center',
+              }}
+            >
+              {/* 마이너스 버튼 */}
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#EBEBEB',
+                  width: 30,
+                  height: 30,
+                  borderRadius: 30,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                  if (personnel > 0) {
+                    setPersonnel(personnel - 1);
+                  }
+                }}
+              >
+                <Entypo name="minus" size={12} color="#777" />
+              </TouchableOpacity>
+
+              {/* 모집인원 */}
+              <Text
+                style={{
+                  flex: 1,
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                }}
+              >
+                {personnel}
+              </Text>
+
+              {/* 플러스 버튼 */}
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#EBEBEB',
+                  width: 30,
+                  height: 30,
+                  borderRadius: 30,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                  setPersonnel(personnel + 1);
+                }}
+              >
+                <Entypo name="plus" size={12} color="#777" />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* 시작일 */}
+          <View
+            style={{
+              flexDirection: 'row',
+              marginBottom: 20,
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 2, marginEnd: 20 }}>
+              <Text style={styles.label}>시작일</Text>
+            </View>
+            <TouchableOpacity
+              style={{
+                flex: 5,
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                borderRadius: 30,
+                borderWidth: 2,
+                borderColor: '#CBCBCB',
+              }}
+              onPress={() => {
+                setCurrentModal('start');
+                setModalOpen(true);
+              }}
+            >
+              <TextInput
+                style={{ color: '#111' }}
+                editable={false}
+                placeholder={'시작 일시를 선택하세요.'}
+                placeholderTextColor={'#999'}
+                value={startDate}
               />
-            </View>
+            </TouchableOpacity>
           </View>
 
-          {/* 스터디 기간 */}
-          <View style={styles.row}>
-            {/* 시작 일시 */}
-            <View style={{ width: '47.5%', marginBottom: 20 }}>
-              <Text style={styles.label}>시작 일시</Text>
-              <TouchableOpacity
-                style={styles.inputBox}
-                onPress={() => {
-                  setCurrentModal('start');
-                  setModalOpen(true);
-                }}
-              >
-                <TextInput
-                  style={{ color: '#111' }}
-                  editable={false}
-                  placeholder={'시작 일시를 선택하세요.'}
-                  placeholderTextColor={'#999'}
-                  value={startDate}
-                />
-              </TouchableOpacity>
+          {/* 종료일 */}
+          <View
+            style={{
+              flexDirection: 'row',
+              marginBottom: 20,
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 2, marginEnd: 20 }}>
+              <Text style={styles.label}>종료일</Text>
             </View>
-
-            {/* 종료 일시 */}
-            <View style={{ width: '47.5%', marginBottom: 20 }}>
-              <Text style={styles.label}>종료 일시</Text>
-              <TouchableOpacity
-                style={styles.inputBox}
-                onPress={() => {
-                  setCurrentModal('due');
-                  setModalOpen(true);
-                }}
-              >
-                <TextInput
-                  style={{ color: '#111' }}
-                  editable={false}
-                  placeholder={'종료 일시를 선택하세요.'}
-                  placeholderTextColor={'#999'}
-                  value={dueDate}
-                />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity
+              style={{
+                flex: 5,
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                borderRadius: 30,
+                borderWidth: 2,
+                borderColor: '#CBCBCB',
+              }}
+              onPress={() => {
+                setCurrentModal('due');
+                setModalOpen(true);
+              }}
+            >
+              <TextInput
+                style={{ color: '#111' }}
+                editable={false}
+                placeholder={'종료 일시를 선택하세요.'}
+                placeholderTextColor={'#999'}
+                value={dueDate}
+              />
+            </TouchableOpacity>
           </View>
+
           {showModal()}
 
           {/* 포지션 / 사용언어 */}
           <View style={styles.row}>
             <View style={{ width: '47.5%' }}>
               {/* 작성자 포지션 */}
-
               <SignInput
                 style={{ flex: 3 }}
                 label={'작성자 포지션'}
@@ -230,6 +383,7 @@ export default function CreatePost({ navigation }) {
                 setValue={setPosition}
               />
             </View>
+
             {/* 작성자 사용 언어 */}
             <View style={{ width: '47.5%' }}>
               <SignInput
@@ -242,6 +396,15 @@ export default function CreatePost({ navigation }) {
               />
             </View>
           </View>
+
+          {/* 제목 */}
+          <SignInput
+            label={'제목'}
+            value={title}
+            type={'title'}
+            hint={'제목을 입력하세요.'}
+            setValue={setTitle}
+          />
 
           {/* 소개글 */}
           <View style={{ width: '100%', paddingBottom: 20 }}>
@@ -277,24 +440,11 @@ export default function CreatePost({ navigation }) {
           </View>
 
           {/* 선택된 해시태그 */}
-          <View style={{ flexDirection: 'row', marginVertical: 10 }}>
-            {hashtagList.map((tag, i) => {
-              return <HashtagButton title={tag} key={i} />;
-            })}
-          </View>
+          {showSelectedHashtag()}
 
           {/* 해시태그 */}
           <View style={styles.row}>
             <View style={{ width: '100%', paddingBottom: 20 }}>
-              <Text
-                style={{
-                  color: '#263238',
-                  fontWeight: 'bold',
-                  marginVertical: 10,
-                }}
-              >
-                해시태그
-              </Text>
               <View
                 style={{
                   flexDirection: 'row',
@@ -304,14 +454,12 @@ export default function CreatePost({ navigation }) {
                 <View
                   style={{
                     width: '80%',
-                    padding: 10,
-                    borderWidth: 1,
-                    borderColor: '#999',
-                    borderRadius: 4,
+                    borderBottomWidth: 2,
+                    borderColor: '#777',
                   }}
                 >
                   <TextInput
-                    placeholder={'해시태그를 입력하세요.'}
+                    placeholder={'해시태그를 입력하세요. (최대 5개)'}
                     value={hashtag}
                     onChangeText={(text) => {
                       setHashtag(text);
@@ -322,9 +470,8 @@ export default function CreatePost({ navigation }) {
               </View>
             </View>
           </View>
-
-          {showSubmitButton()}
         </View>
+        {showSubmitButton()}
       </ScrollView>
     </View>
   );
@@ -332,13 +479,13 @@ export default function CreatePost({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FFF',
     flex: 1,
     marginTop: getStatusBarHeight(),
   },
   content: {
     flex: 1,
-    backgroundColor: '#FFF',
-    padding: 20,
+    paddingHorizontal: 20,
   },
   row: { flexDirection: 'row', justifyContent: 'space-between' },
   label: {
@@ -358,8 +505,9 @@ const styles = StyleSheet.create({
     minHeight: 25,
   },
   pickerItem: {
-    height: 44,
     color: 'red',
+    height: 44,
+    fontSize: 20,
   },
 
   contentInputContainer: {
@@ -372,12 +520,14 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    backgroundColor: '#1EA7F8',
+    backgroundColor: '#777',
+    padding: 8,
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
   },
   buttonText: {
     color: 'white',
+    fontWeight: 'bold',
   },
 });
