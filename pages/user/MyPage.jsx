7c01ca91a055cb5c24e4be8f92748eb1f90fe22c
hyeonginju <guddls64@naver.com>
2021-05-10 15:17:00 +0900

    fix: MyPage

--- pages/user/MyPage.jsx
+++ pages/user/MyPage.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
 
 import { HeaderSetting } from '../../components/header';
 import { SettingModal } from '../../components/modal';
 
+import Recruit from '../../assets/Recruit.png';
+
 export default function MyPage({ navigation }) {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const imageUpload = () => {};
   return (
     <View style={styles.container}>
       <HeaderSetting
@@ -23,11 +31,52 @@ export default function MyPage({ navigation }) {
       />
 
       <View style={styles.content}>
-        <TouchableOpacity onPress={imageUpload}>
-          <View style={{ width: 100, height: 50, borderWidth: 1, margin: 100 }}>
-            <Text>사진업로드</Text>
+        {/* 프로필 */}
+        <TouchableOpacity style={styles.profile}>
+          <View style={styles.imgFrame}>
+            <Image
+              style={styles.img}
+              source={{
+                uri:
+                  'https://image.news1.kr/system/photos/2020/5/29/4215665/article.jpg/dims/optimize',
+              }}
+            />
+          </View>
+          <View style={styles.nameBox}>
+            <Text style={styles.nameText}>이지은</Text>
+            <Text style={styles.roleText}>개발자</Text>
           </View>
         </TouchableOpacity>
+
+        <View style={styles.studyAll}>
+          <TouchableOpacity style={styles.studyBox}>
+            <Image source={Recruit} />
+            <View style={styles.textBox}>
+              <Text style={styles.cateText}>모집</Text>
+              <Text style={styles.studyText}> 스터디</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.studyBox}>
+            <Image source={Recruit} />
+            <View style={styles.textBox}>
+              <Text style={styles.cateText}>신청</Text>
+              <Text style={styles.studyText}> 스터디</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.studyBox}>
+            <Image source={Recruit} />
+            <View style={styles.textBox}>
+              <Text style={styles.cateText}>저장</Text>
+              <Text style={styles.studyText}> 스터디</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView style={styles.myBox}>
+          <Text>hi</Text>
+        </ScrollView>
       </View>
     </View>
   );
@@ -39,7 +88,46 @@ const styles = StyleSheet.create({
     marginTop: getStatusBarHeight(),
     backgroundColor: 'white',
   },
-  content: {
+  content: { padding: 10 },
+  profile: {
+    flexDirection: 'row',
     alignItems: 'center',
+    padding: 20,
+    marginBottom: 10,
+  },
+  imgFrame: {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 100,
+  },
+  nameBox: { flexDirection: 'row', marginLeft: 20, alignItems: 'center' },
+  nameText: { fontSize: 23, fontWeight: 'bold' },
+  roleText: { fontSize: 15, color: 'grey', marginLeft: 5 },
+  studyAll: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingHorizontal: 10,
+  },
+  studyBox: { alignItems: 'center' },
+  textBox: { flexDirection: 'row', paddingTop: 20 },
+  cateText: { fontWeight: 'bold' },
+  studyText: { color: 'grey' },
+  myBox: {
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 5,
+    margin: 20,
+    marginTop: 30,
+    padding: 20,
+    height: '40%',
   },
 });
