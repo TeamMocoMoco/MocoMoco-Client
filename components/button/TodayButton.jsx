0648f3f87599a3522399d547178fb8fcf99b287b
hyeonginju <guddls64@naver.com>
2021-05-12 11:39:29 +0900

    add: TodayButton

---
+++ components/button/TodayButton.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { getColor } from '../../styles/styles';
+
+export default function TodayButton({ title, today, setToday }) {
+  if (title == today) {
+    return (
+      <TouchableOpacity disabled style={styles.active}>
+        <Text style={{ color: '#FFF', fontSize: 14, fontWeight: 'bold' }}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={styles.inactive}
+        onPress={() => {
+          setToday(title);
+        }}
+      >
+        <Text style={{ color: '#8E8E8E', fontSize: 14 }}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  active: {
+    backgroundColor: getColor('defaultColor'),
+    flex: 1,
+    padding: 10,
+    marginHorizontal: 5,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: getColor('defaultColor'),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inactive: {
+    backgroundColor: getColor('inactiveColor'),
+    flex: 1,
+    padding: 10,
+    marginHorizontal: 5,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: getColor('inactiveColor'),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#FFF',
+  },
+});
