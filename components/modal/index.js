70621ffb0d4e452c011d57339ea880c0e344eaaa
hyeonginju <guddls64@naver.com>
2021-05-12 11:40:16 +0900

    feat: DatePicker

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,9 +1,11 @@
 import DatePickModal from './DatePickModal';
 import DotModal from './DotModal';
 import SettingModal from './SettingModal';
+import TimePickModal from './TimePickModal';
 
 module.exports = {
   DatePickModal,
   DotModal,
   SettingModal,
+  TimePickModal,
 };
