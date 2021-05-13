0e33a4a8f59095e1cdb1b8c3111b5c53095e2c4b
Delilah1004 <pskdy8414@naver.com>
2021-05-14 02:22:43 +0900

    remove: timepicker

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,11 +1,9 @@
 import DatePickModal from './DatePickModal';
 import DotModal from './DotModal';
 import SettingModal from './SettingModal';
-import TimePickModal from './TimePickModal';
 
 module.exports = {
   DatePickModal,
   DotModal,
   SettingModal,
-  TimePickModal,
 };
