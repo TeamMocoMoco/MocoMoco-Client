63d5ac58691cbe953df0e2005b101f6524cc525b
Delilah1004 <pskdy8414@naver.com>
2021-05-18 20:30:14 +0900

    fix: 검색 기능 수정

--- pages/post/SubList.jsx
+++ pages/post/SubList.jsx
@@ -5,8 +5,11 @@ import {
   FlatList,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
+
+import { Entypo } from '@expo/vector-icons';
 
 import { HeaderBack } from '../../components/header';
 import { MainCard } from '../../components/card';
@@ -17,31 +20,32 @@ import {
 } from '../../config/api/PostAPI';
 
 import { getColor } from '../../styles/styles';
+import SearchBar from '../../components/input/SearchBar';
 
 export default function SubList({ navigation, route }) {
-  const keyword = route.params.value;
   const meeting = route.params.tab;
 
   const pageNum = useRef(1);
   const flatListRef = useRef();
 
   const [ready, setReady] = useState(false);
+  const [keyword, setKeyword] = useState(route.params.value);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     navigation.addListener('focus', (e) => {
       setTimeout(() => {
         setReady(false);
-        download(meeting);
+        download(keyword);
         setReady(true);
       });
     });
   }, [navigation]);
 
-  const download = useCallback(async (title) => {
+  const download = useCallback(async (keyword) => {
     pageNum.current = 1;
     let result = [];
-    if (title == '전체보기') {
+    if (meeting == '전체보기') {
       result = await getPostsByKeyword(keyword, pageNum.current);
     } else {
       result = await getPostsByMeetingByKeyword(
@@ -55,8 +59,21 @@ export default function SubList({ navigation, route }) {
 
   return ready ? (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput disabled style={styles.input} value={keyword} />
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity
+          style={{ paddingVertical: 10, paddingEnd: 10 }}
+          onPress={() => navigation.goBack()}
+        >
+          <Entypo name="chevron-small-left" size={30} color="black" />
+        </TouchableOpacity>
+        <SearchBar
+          hint={'검색어를 입력하세요'}
+          keyword={route.params.value}
+          doFunction={(value) => {
+            download(value);
+            setKeyword(value);
+          }}
+        />
       </View>
       <View style={styles.content}>
         <FlatList
