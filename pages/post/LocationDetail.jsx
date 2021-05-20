269fffb59edf08479baaf33139d0a5d740b59e1e
Delilah1004 <pskdy8414@naver.com>
2021-05-21 01:21:25 +0900

    feat: 위치 확인 페이지 뒤로가기 기능 추가

--- pages/post/LocationDetail.jsx
+++ pages/post/LocationDetail.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-import MapView, { Marker, Circle, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Circle, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import { getColor } from '../../styles/styles';
-import { getPostsByLocation } from '../../config/api/MapAPI';
+import { Entypo } from '@expo/vector-icons';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const ASPECT_RATIO = windowWidth / windowHeight;
-const LATITUDE_DELTA = 0.02;
+const LATITUDE_DELTA = 0.01;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default function LocationDetail({ navigation, route }) {
   const location = route.params;
 
-  const mapRef = useRef(null);
-
-  const [mapCenter, setMapCenter] = useState([location[0], location[1]]);
-
   return (
     <View style={styles.container}>
       {/* 지도 */}
       <MapView
-        ref={mapRef}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: mapCenter[0],
-          longitude: mapCenter[1],
+          latitude: location[0],
+          longitude: location[1],
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA,
         }}
@@ -40,11 +40,27 @@ export default function LocationDetail({ navigation, route }) {
             latitude: location[0],
             longitude: location[1],
           }}
-          radius={windowWidth / 10}
-          strokeWidth={0}
+          radius={windowWidth / 4}
+          strokeWidth={10}
+          strokeColor={'#FFFFFF99'}
           fillColor={'#435BEF99'}
         />
       </MapView>
+      <TouchableOpacity
+        style={{
+          flexDirection: 'row',
+          position: 'absolute',
+          top: 20,
+          left: 20,
+          alignItems: 'center',
+        }}
+        onPress={() => navigation.goBack()}
+      >
+        <Entypo name="chevron-small-left" size={40} color="black" />
+        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+          게시글로 돌아가기
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
