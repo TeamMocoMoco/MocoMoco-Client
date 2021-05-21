9413c997a80d2ce8d127b0e0ac4e636efe331158
Delilah1004 <pskdy8414@naver.com>
2021-05-21 18:02:57 +0900

    remove: 필요없는 Button Component 삭제

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,4 +1,3 @@
-import MainCategory from './MainCategory';
 import MainList from './MainList';
 import SubList from './SubList';
 import SearchMap from './SearchMap';
@@ -9,7 +8,6 @@ import ReadPost from './ReadPost';
 import UpdatePost from './UpdatePost';
 
 module.exports = {
-  MainCategory,
   MainList,
   SubList,
   SearchMap,
