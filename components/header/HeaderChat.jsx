be3f1c77c12dfa513c3267c33d81017d427a1021
hyeonginju <guddls64@naver.com>
2021-05-26 12:44:16 +0900

    feat: headerChat

--- components/header/HeaderChat.jsx
+++ components/header/HeaderChat.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   TouchableOpacity,
   Dimensions,
+  Image,
 } from 'react-native';
 
 import { getColor } from '../../styles/styles';
@@ -12,8 +13,15 @@ import { getColor } from '../../styles/styles';
 import { Entypo } from '@expo/vector-icons';
 
 const windowHeight = Dimensions.get('window').height;
+const diviceWidth = Dimensions.get('window').width;
 
-export default function HeaderChat({ navigation, name, outRoom }) {
+export default function HeaderChat({
+  navigation,
+  name,
+  outRoom,
+  participantInfo,
+  adminInfo,
+}) {
   const [lastPress, setLastPress] = useState(false);
 
   useEffect(() => {
@@ -30,7 +38,7 @@ export default function HeaderChat({ navigation, name, outRoom }) {
     }
   };
 
-  return (
+  return participantInfo.name == name ? (
     <View style={styles.header}>
       <View style={styles.startBox}>
         <Entypo
@@ -41,7 +49,51 @@ export default function HeaderChat({ navigation, name, outRoom }) {
         />
       </View>
 
-      <Text style={styles.headerText}>{name}</Text>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.push('OtherProfile', (navigation, participantInfo._id))
+        }
+        style={styles.nameBox}
+      >
+        <Image
+          source={{ uri: participantInfo.userImg }}
+          style={[styles.profileImage]}
+        />
+        <Text style={styles.headerText}>{name}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          outRoom();
+        }}
+        style={styles.endBox}
+      >
+        <Text style={styles.outText}>나가기</Text>
+      </TouchableOpacity>
+    </View>
+  ) : (
+    <View style={styles.header}>
+      <View style={styles.startBox}>
+        <Entypo
+          name="chevron-small-left"
+          size={35}
+          color="black"
+          onPress={() => back()}
+        />
+      </View>
+
+      <TouchableOpacity
+        onPress={() =>
+          navigation.push('OtherProfile', (navigation, adminInfo._id))
+        }
+        style={styles.nameBox}
+      >
+        <Image
+          source={{ uri: adminInfo.userImg }}
+          style={[styles.profileImage]}
+        />
+        <Text style={styles.headerText}>{name}</Text>
+      </TouchableOpacity>
 
       <TouchableOpacity
         onPress={() => {
@@ -64,18 +116,29 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: getColor('HeaderBorderColor'),
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  startBox: {
+    justifyContent: 'flex-start',
+  },
+  nameBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileImage: {
+    resizeMode: 'cover',
+    height: diviceWidth * 0.1,
+    width: diviceWidth * 0.1,
+    borderRadius: 100,
+    marginRight: 7,
   },
   headerText: {
-    flex: 5,
     color: '#000',
     fontSize: 17,
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  startBox: {
-    flex: 1,
-    justifyContent: 'flex-start',
-  },
-  endBox: { flex: 1, justifyContent: 'flex-end' },
+  endBox: { justifyContent: 'flex-end' },
   outText: { color: '#8E9297' },
 });
