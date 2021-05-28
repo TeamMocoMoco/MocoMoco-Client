69b5333606fe0d873e215d4b2e5712b1484856be
Delilah1004 <pskdy8414@naver.com>
2021-05-28 18:08:31 +0900

    fix: iOS 데이터 삭제 오류 해결

--- config/api/ChatAPI.js
+++ config/api/ChatAPI.js
@@ -132,6 +132,9 @@ export async function getMyRooms() {
       error = error.substr(7);
     }
 
+    await SecureStore.deleteItemAsync('usertoken');
+    await AsyncStorage.clear();
+
     Alert.alert(error);
   }
 }
